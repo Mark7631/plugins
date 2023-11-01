946801f1a52ce785b8d332a649ea92a76ec38065
Mark7631 <lmark.gm@gmail.com>
2023-11-01 17:12:13 +0300

    top1

--- test/simplePage.go
+++ test/simplePage.go
@@ -1,9 +1,23 @@
-package test
+package main
 
 import (
-	"fmt"
+	"github.com/apache/incubator-answer-plugins/editor-formula/i18n"
+	"github.com/answerdev/answer/plugin"
 )
 
+type FormulaPlugin struct {
+}
+
 func init() {
-	fmt.Println("test init")
+	plugin.Register(&FormulaPlugin{})
+}
+
+func (d FormulaPlugin) Info() plugin.Info {
+	return plugin.Info{
+		Name:        plugin.MakeTranslator(i18n.InfoName),
+		SlugName:    "karma",
+		Description: plugin.MakeTranslator(i18n.InfoDescription),
+		Author:      "stradalez",
+		Version:     "0.0.1",
+	}
 }
